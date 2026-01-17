9a80c178108a3437fc6f6aea73505c9febaefadb
Sergei Rozanov <laska4723@yandex.ru>
2026-01-18 02:37:37 +0300

    Числа. Задача 1. Генератор

--- src/main.js
+++ src/main.js
@@ -1,26 +1,3 @@
-import { lenText, formatText, isStringStartsWith, getWelcomeMessage, logStringInfo } from './strings.js';
+import { generateInteger } from './numbers.js';
 
-console.log(lenText('Привет!'));
-console.log(lenText('Это очень длинная строка :)'));
-
-console.log();
-
-console.log(formatText('МОЛОКО'));
-console.log(formatText('картошка'));
-console.log(formatText('СтРаНнОе СлОвО'));
-
-console.log();
-
-console.log(isStringStartsWith('Ветеринар', 'вет'));
-console.log(isStringStartsWith('Молоко', 'мол'));
-console.log(isStringStartsWith('Карго Корги', 'КАРГО '));
-console.log(isStringStartsWith('Карго Корги', 'кор '));
-console.log(isStringStartsWith('Ковёр', 'кова '));
-
-console.log(' ');
-
-console.log(getWelcomeMessage('Сергей', 28));
-
-console.log(' ');
-
-console.log(logStringInfo('Гренландия'));
+console.log(generateInteger(0, 5));
