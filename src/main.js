3c2358602affcb5baeb2260558af21fcd123fc6b
Sergei Rozanov <laska4723@yandex.ru>
2026-01-17 11:44:46 +0300

    Строки. Задача 2. Первый и последний символ строки

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { len } from './strings.js';
+import { len, func } from './strings.js';
 
 console.log(len('Привет!'));
 console.log(len('Это очень длинная строка :)'));
+
+console.log();
+
+console.log(func('МОЛОКО'));
+console.log(func('картошка'));
+console.log(func('СтРаНнОе СлОвО'));
--- src/strings.js
+++ src/strings.js
@@ -1,3 +1,13 @@
+import chalk from 'chalk';
+
 export const len = (str) => {
   return str.length;
+};
+
+export const func = (str) => {
+  const lower = str.toLowerCase();
+  const green = chalk.green;
+  const blue = chalk.blue;
+
+  return green(lower[0].toUpperCase()) + blue(lower.slice(1, -1)) + green(lower.at(-1).toUpperCase());
 };
