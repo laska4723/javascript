051e8849b32e1914909c73de87dbfb0f7e227d58
Sergei Rozanov <laska4723@yandex.ru>
2026-01-17 07:45:01 +0300

    Функции. Задача 2. Цена томатов

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { sayHi, sayText, sum, power } from './functions.js';
+import { sayHi, sayText, sum, power, tomatoPrice } from './functions.js';
 
 console.log(sayHi('Сергей'));
 console.log(sayText('Машина'));
 console.log(sum(1, 2, 3, 4, 5));
 console.log(power(2, 3));
+
+console.log();
+
+console.log(tomatoPrice(5, 650));
