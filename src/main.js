e0f6db797af897055a5ecea4e0c3db0871f296f9
Sergei Rozanov <laska4723@yandex.ru>
2026-01-17 11:34:30 +0300

    Строки. Задача 1. Длина строки

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
-import { sayHi, sayText, sum, power, tomatoPrice } from './functions.js';
+import { len } from './strings.js';
 
-console.log(sayHi('Сергей'));
-console.log(sayText('Машина'));
-console.log(sum(1, 2, 3, 4, 5));
-console.log(power(2, 3));
-
-console.log();
-
-console.log(tomatoPrice(5, 650));
+console.log(len('Привет!'));
+console.log(len('Это очень длинная строка :)'));
