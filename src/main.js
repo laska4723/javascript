4e15ddf179f6905652ddd75dc299ebac7b8bfb55
Sergei Rozanov <laska4723@yandex.ru>
2026-01-18 03:19:43 +0300

    Числа. Задача 3. Площадь круга

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { generateInteger, getSquareArea } from './numbers.js';
+import { generateInteger, getSquareArea, getCircleArea } from './numbers.js';
 
 console.log(generateInteger(0, 5));
 
@@ -7,3 +7,9 @@ console.log();
 const squareSide = generateInteger(1, 11);
 const squareArea = getSquareArea(squareSide);
 console.log(`Площадь квадрата со стороной a=${squareSide} равна ${squareArea}`);
+
+console.log();
+
+const circleRadius = generateInteger(3, 13);
+const circleArea = getCircleArea(circleRadius);
+console.log(`Радиус: ${circleRadius}\nПлощадь: ${circleArea}`);
--- src/numbers.js
+++ src/numbers.js
@@ -5,3 +5,9 @@ export const generateInteger = (min, max) => {
 export const getSquareArea = (side) => {
   return side * side;
 };
+
+export const getCircleArea = (radius) => {
+  const area = Math.PI * radius ** 2;
+
+  return Number(area.toFixed(2));
+};
