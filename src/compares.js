3a52e4698aa6042a2e37dc15c015d4af3a5b1319
Sergei Rozanov <laska4723@yandex.ru>
2026-01-19 23:09:40 +0300

    Сравнение. Отрицание. Задача 2. Сравнение строк

--- src/compares.js
+++ src/compares.js
@@ -1,3 +1,7 @@
 export const compare = (a, b) => {
   return a === b;
 };
+
+export const compareStrings = (a, b) => {
+  return a.toLowerCase() === b.toLowerCase();
+};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { compare } from './compares.js';
+import { compare, compareStrings } from './compares.js';
 
 console.log(compare(1, 1));
 console.log(compare(1, '1'));
@@ -7,3 +7,9 @@ console.log(compare(false, false));
 console.log(compare(23, 23));
 console.log(compare('abc', 'abc'));
 console.log(compare('ABC', 'abc'));
+
+console.log();
+
+console.log(compareStrings('1', '1'));
+console.log(compareStrings('abc', 'abc'));
+console.log(compareStrings('ABC', 'abc'));
