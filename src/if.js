c7feaebde6bcf2b089c3011ca5f433ec60daf24d
Sergei Rozanov <laska4723@yandex.ru>
2026-01-20 09:12:24 +0300

    Условия. Задача 2. FizzBuzz

--- src/if.js
+++ src/if.js
@@ -7,3 +7,11 @@ export const isAgeValid = (age) => {
     return chalk.red('false');
   }
 };
+
+export const fizzBuzz = (number) => {
+  if (number % 3 === 0 && number % 5 === 0) return 'FizzBuzz';
+  if (number % 3 === 0) return 'Fizz';
+  if (number % 5 === 0) return 'Buzz';
+
+  return number;
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { isAgeValid } from './if.js';
 import { generateInteger } from './numbers.js';
+import { isAgeValid, fizzBuzz } from './if.js';
 
 const age = generateInteger(15, 23);
 console.log(`Возраст: ${age}\n${isAgeValid(age)}`);
+
+console.log();
+
+const number = generateInteger(9, 21);
+console.log(`Число: ${number}\n${fizzBuzz(number)}`);
