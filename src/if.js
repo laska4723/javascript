f9f34609e749b44a64975eb3ba05afcbce50e8a6
Sergei Rozanov <laska4723@yandex.ru>
2026-01-20 09:57:59 +0300

    Условия. Задача 3. Високосный год (fix: 1)

--- src/if.js
+++ src/if.js
@@ -25,8 +25,8 @@ export const generateRandomYear = () => {
 
 export const isYearLeap = (year) => {
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    return 'високосный год';
+    return 'true';
   }
 
-  return 'не високосный год';
+  return 'false';
 };
