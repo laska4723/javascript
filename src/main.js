c01f278be21cbf8043a37dd29a3ea61d4375cf1f
Sergei Rozanov <laska4723@yandex.ru>
2026-01-21 12:19:57 +0300

    Объекты. Задача 2. Удаление поля (fix: 1)

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ console.log();
 
 const user1 = { id: 1, age: 20, name: 'alex' };
 removeProperty(user1, 'id');
-console.log(user1);
+console.log(user1); // { age: 20, name: 'alex' }
 removeProperty(user1, 'age');
-console.log(user1);
+console.log(user1); // { name: 'alex' }
 removeProperty(user1, 'name');
-console.log(user1);
+console.log(user1); // {}
