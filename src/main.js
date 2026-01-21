7fc8f13fcd23733be30ab906b24a31e7c1d54354
Sergei Rozanov <laska4723@yandex.ru>
2026-01-21 05:46:21 +0300

    Switch. Задача 2. Маппер для статуса ресторана

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { spongeBob } from './switches.js';
+import { showOrderStatus, spongeBob } from './switches.js';
 
 spongeBob('spongebob');
 spongeBob('patrick');
@@ -6,3 +6,11 @@ spongeBob('squidward');
 spongeBob('plankton');
 spongeBob('mr. crabs');
 spongeBob('kraken');
+
+console.log();
+
+showOrderStatus('Формируются чеки');
+showOrderStatus('Варится');
+showOrderStatus('Курьер подъезжает');
+showOrderStatus('Клиент отказался от заказа');
+showOrderStatus('Обрабатывается');
--- src/switches.js
+++ src/switches.js
@@ -26,3 +26,34 @@ export const spongeBob = (character) => {
       console.log('Неизвестный персонаж');
   }
 };
+
+export const showOrderStatus = (orderStatus) => {
+  switch (orderStatus) {
+    case 'Формируются чеки':
+    case 'Назначаются повара':
+      console.log(chalk.blue('Обрабатывается'));
+      break;
+
+    case 'Запекается':
+    case 'Нарезается':
+    case 'Варится':
+    case 'Жарится':
+    case 'Поиск курьера':
+      console.log(chalk.yellow('Готовится'));
+      break;
+
+    case 'Курьер берёт заказ':
+    case 'Курьер в пути':
+    case 'Курьер подъезжает':
+      console.log(chalk.blue('У курьера'));
+      break;
+
+    case 'Клиент отказался от заказа':
+    case 'Заказ доставлен':
+      console.log(chalk.green('Готов'));
+      break;
+
+    default:
+      console.log(chalk.white('Обрабатывается'));
+  }
+};
